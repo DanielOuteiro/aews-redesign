82452fe140fd8756b977d0835509a9ec3c2ba8c9
Kyle McDonald <kyle@kylemcdonald.net>
2026-04-30 12:06:56 -0700

    Move emergency level note to hero copy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1440,8 +1440,7 @@ function AboutSystemCard() {
       <div className="about-copy">
         <p>
           This site watches a fixed cohort of business jets and asks a simple question: is the number currently airborne
-          unusual for this time? The current emergency level is reported on a scale of 1 to 5, with 5 being an
-          indicator of a likely imminent apocalypse. It is not tracking all aircraft. The tracked set is built from{' '}
+          unusual for this time? It is not tracking all aircraft. The tracked set is built from{' '}
           <a
             href="https://www.faa.gov/licenses_certificates/aircraft_certification/aircraft_registry/releasable_aircraft_download"
             target="_blank"
@@ -1673,6 +1672,8 @@ function App() {
             <p className="hero-caption">
               In the event of an imminent nuclear apocalypse, we suspect that many people who have access to private jets
               will immediately take to the skies and escape city centers. This site tracks this indicator in realtime.
+              The current emergency level is reported on a scale of 1 to 5, with 5 being an indicator of a likely
+              imminent apocalypse.
             </p>
           </section>
           <div className="dial-stack">
